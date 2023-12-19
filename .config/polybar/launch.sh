18a6e875cd50dc8610a51347b63e549f1754b125
Cyrus Duong <cyrusduong@gmail.com>
2023-12-19 00:45:32 -0600

    fix screen startup on login/xinitrc/startx and tray icon to primary, delay startup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
-
-# Launch polybar
-# echo "---" | tee -a /tmp/polybar.log
-# polybar main 2>&1 | tee -a /tmp/polybar.log &
-# disown
+killall -q polybar
+sleep 0.25
 
 if type "xrandr"; then
-	for m in $(xrandr --listmonitors | rg - | cut --delimiter=" " --fields=6); do
+	for m in $(xrandr --listmonitors | awk '{print $4}'); do
 		MONITOR=$m polybar --reload main 2>&1 | tee -a "/tmp/polybar-$m" &
-		sleep 0.05 # Added to always show systray items on primary bar when reloading
+		sleep 0.1 # Added to always show systray items on primary bar when reloading
 	done
 else
 	polybar --reload example &
