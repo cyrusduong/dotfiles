534fa8ad71f1e668f6395460041008eaca05f7b4
Cyrus Duong <cyrusduong@gmail.com>
2023-12-19 00:02:57 -0600

    polybar + pulseaudio-control + spotify = fire

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ ESSENTIALS2=("wget gawk wireplumber xorg-xev git pipewire rsync rclone pipewire-
 EXTRAS=("neovim firefox chromium rofi rustup meld aws-cli transmission-cli transmission-qt wezterm copyq otf-monaspace-nerd helvum bat spotifyd bottom htop")
 EXTRAS2=("remmina feh krita gimp rawtherapee nemo xcolor xplr fzf lazygit github-cli hub jq gron bottom glances neofetch font-manager dust peco gradle")
 EXTRAS3=("bitwarden-cli bitwarden polybar arandr autorandr yadm discord tealdeer hacksaw shotgun")
-AUR=("slack-desktop usbimager android-studio i3lock-color spotify zoom pulseaudio-control")
+AUR=("slack-desktop usbimager android-studio i3lock-color spotify zoom pulseaudio-control zscroll-git")
 
 # Ask for sudo
 if [ $EUID != 0 ]; then
