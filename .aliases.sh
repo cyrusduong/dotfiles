35eca7204899129bd51528e133bbd4422debc0dd
Cyrus Duong <cyrusduong@gmail.com>
2023-12-20 23:48:10 -0600

    update picom values and aliases for keeb maint

--- .aliases.sh
+++ .aliases.sh
@@ -19,7 +19,14 @@ alias sources='v ~/.sources.sh'
 alias i3config='v ~/.config/i3/config'
 alias vconfig='v ~/.config/nvim'
 alias barconfig='v ~/.config/polybar/config.ini'
+alias picomconfig='v ~/.config/picom/picom.conf'
 alias zshconfig='v ~/.zshrc'
+
+# for keyboard
+alias keymap='v ~/personal/lily58-zmk-config/config/lily58.keymap'
+alias keeb_dl='. ~/personal/lily58-zmk-config/download.sh'
+alias keeb_in='. ~/personal/lily58-zmk-config/install.sh'
+alias keeb_up='keeb_dl && keeb_in'
 
 alias arst='asdf'
 
