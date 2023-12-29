ad156a13729fa0f8bcb2b41ef5d465666412f3d7
Cyrus Duong <cyrusduong@gmail.com>
2023-12-29 14:54:31 -0600

    add missing images, update bootstrap to not worry about firmware

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 
 # Add packages wanted in bootstrap
-ESSENTIALS=("base-devel btrfs-progs exfatprogs e2fsprogs pacman-contrib xorg-xinit i3-wm dunst man-db man-pages inetutils amd-ucode fd unzip xorg-xkill pango xclip")
-ESSENTIALS2=("wget gawk wireplumber xorg-xev git pipewire rsync rclone pipewire-audio pipewire-pulse ripgrep playerctl gnome-keyring seahorse")
-EXTRAS=("neovim firefox chromium rofi rustup meld aws-cli transmission-cli transmission-qt wezterm copyq otf-monaspace-nerd helvum bat bottom htop")
+ESSENTIALS1=("base-devel btrfs-progs exfatprogs e2fsprogs pacman-contrib xorg-xinit i3-wm dunst man-db man-pages inetutils fd unzip xorg-xkill pango xclip")
+ESSENTIALS2=("wget gawk wireplumber xorg-xev git pipewire rsync rclone pipewire-audio pipewire-pulse ripgrep playerctl gnome-keyring seahorse noto-fonts zsh")
+EXTRAS1=("neovim firefox chromium rofi rustup meld aws-cli transmission-cli transmission-qt wezterm copyq otf-monaspace-nerd helvum bat bottom htop")
 EXTRAS2=("remmina feh krita gimp rawtherapee nemo xcolor xplr fzf lazygit github-cli hub jq gron bottom glances neofetch font-manager dust peco gradle")
-EXTRAS3=("bitwarden-cli bitwarden polybar arandr autorandr yadm discord tealdeer hacksaw shotgun papirus-icon-theme rsync figlet ttf-hanazono ncurses xorg-xsetroot xorg-xwininfo")
-EXTRAS3=("httpie flowblade obs-studio")
-AUR=("slack-desktop usbimager android-studio spotify zoom pulseaudio-control zscroll-git postman-bin neo-matrix compfy")
+EXTRAS3=("bitwarden-cli bitwarden polybar arandr autorandr yadm discord tealdeer hacksaw shotgun papirus-icon-theme rsync figlet ttf-hanazono")
+EXTRAS3=("ncurses xorg-xsetroot xorg-xwininfo httpie flowblade obs-studio")
+AUR1=("slack-desktop usbimager android-studio spotify zoom pulseaudio-control zscroll-git postman-bin neo-matrix compfy")
 AUR2=("rtx starship-git wakeonlan ncspot-bin xplorer-bin")
-FINAL="$EXTRAS $EXTRAS2 $EXTRAS3 $AUR $AUR2"
+
+ESSENTIALS="$ESSENTIALS1 $ESSENTIALS2"
+EXTRAS="$EXTRAS1 $EXTRAS2 $EXTRAS3"
+AUR="$AUR1 $AUR2"
 
 # Ask for sudo
 if [ $EUID != 0 ]; then
@@ -39,20 +42,17 @@ echo "Setting up /etc/pacman.conf"
 sed --in-place 's/= 5/= 20/' /etc/pacman.conf
 sed --in-place 's/#Parallel/Parallel/' /etc/pacman.conf
 
-# Get the essentials
-pacman -Syu --noconfirm --needed $ESSENTIALS $ESSENTIALS2
-
-# All the noto fonts (for the unicode support)
-pacman -S $(pacman -Ssq noto-fonts)
-
-# Set pacman cache to clean up after itself weekly
-echo "Enabling paccache.timer"
-systemctl enable paccache.timer
-
 # Setup rust toolchain
 echo "Setting up rust toolchain to stable for this machine"
 rustup toolchain install stable
 rustup default stable
+
+# Get the essentials
+pacman -Syu --noconfirm --needed $ESSENTIALS $EXTRAS
+
+# Set pacman cache to clean up after itself weekly
+echo "Enabling paccache.timer"
+systemctl enable paccache.timer
 
 # Setup paru using $SUDO_USER (so permissions are ok)
 echo "Cloning paru"
@@ -61,26 +61,13 @@ cd paru
 sudo -H -u $SUDO_USER bash -c 'makepkg -si'
 cd ..
 
-# Install PARU list of packages because why not use both?
-echo "Installing PARU defined and AUR packages (see this file to change list of packages installed)"
-sudo -H -u $SUDO_USER bash -c "paru -S --noconfirm --nouseask --sudoloop --needed $EXTRAS $EXTRAS2 $EXTRAS3 $AUR"
+echo "Installing AUR packages"
+sudo -H -u $SUDO_USER bash -c "paru -S --noconfirm --nouseask --sudoloop --needed $AUR"
 
 # Setup polybar config in ~ (should this just be pulled via yadm?)
 echo "Setting up ~/.config/polybar/config.ini file"
 mkdir -p /home/$SUDO_USER/.config/polybar
 cp /etc/polybar/config.ini /home/$SUDO_USER/.config/polybar
-
-# Setup xinit
-#cp /etc/X11/xinit/xinitrc /home/$SUDO_USER/.xinitrc
-
-# Setup lightdm and lightdm-mini-greeter
-# echo "Configuring lightdm and lightdm-mini-greeter"
-# sed --in-place 's/#greeter-session/greeter-session/' /etc/lightdm/lightdm.conf
-# sed --in-place 's/example-gtk-gnome/lightdm-mini-greeter/' /etc/lightdm/lightdm.conf
-# sed --in-place "s/CHANGE_ME/$SUDO_USER/" /etc/lightdm/lightdm-mini-greeter.conf
-
-# Setup change default shell to zsh
-chsh -s $(which zsh) $SUDO_USER
 
 # Install lazyvim
 mv /home/$SUDO_USER/config/nvim{,.bak}
@@ -101,15 +88,15 @@ sudo -H -u $SUDO_USER bash -c "$(systemctl --user enable wireplumber.service)"
 sudo -H -u $SUDO_USER bash -c "$(systemctl --user enable playerctld.service)"
 
 # Conditionally Get pritunl from official pritunl client repo
-sudo tee -a /etc/pacman.conf <<EOF
-[pritunl]
-Server = https://repo.pritunl.com/stable/pacman
-EOF
-
-sudo pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo pacman -Sy
-sudo pacman -S pritunl-client-electron
+# sudo tee -a /etc/pacman.conf <<EOF
+# [pritunl]
+# Server = https://repo.pritunl.com/stable/pacman
+# EOF
+#
+# sudo pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+# sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+# sudo pacman -Sy
+# sudo pacman -S pritunl-client-electron
 
 # Setup keyboard mnt point
 mkdir /mnt/nano
